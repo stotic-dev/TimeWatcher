a9c3e4de4ddfaedc6b18ef015fceb1e5de532705
佐藤汰一 <satoutaichi@satoutaichinoMacBook-Air.local>
2024-09-14 14:29:28 +0900

    add live activity view

---
+++ TimeWatcherPrj/TimeWatcherWidget/TimeWatcherWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TimeWatcherWidgetBundle.swift
+//  TimeWatcherWidget
+//
+//  Created by 佐藤汰一 on 2024/09/13.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TimeWatcherWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TimeWatcherWidgetLiveActivity()
+    }
+}
