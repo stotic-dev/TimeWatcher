6df0ffe4c47dd75fa672ef8ea75f3ece48482e89
佐藤汰一 <satoutaichi@satoutaichinoMacBook-Air.local>
2024-09-21 11:23:06 +0900

    アプリキル時にLiveActivityを終了するように修正

--- TimeWatcherPrj/TimeWatcher/View/RootView/TimeWatcherApp.swift
+++ TimeWatcherPrj/TimeWatcher/View/RootView/TimeWatcherApp.swift
@@ -7,9 +7,22 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+        logger.info("[In]")
+        
+        let liveActivityManager = LiveActivityManager()
+        liveActivityManager.terminate()
+    }
+}
+
 @main
 @MainActor
 struct TimeWatcherApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
     // Deep Linkで開かれた際の情報を持つViewModel
     private var openUrlViewModel = OpenUrlViewModel()
